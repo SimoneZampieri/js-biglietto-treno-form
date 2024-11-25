c53f4bcff6a86bd3fd1341e83bf9be367c149194
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-25 15:51:08 +0100

    [add] img folder

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// const prezzoKm = 0.21;
+// const scontoMinori = 20;
+// const scontoOver = 40;
+
+// //dati da sapere
+
+// const kmTotali = prompt('Quanti KM devi percorrere?');
+// const etaPasseggero = prompt('Quanti anni Hai?');
+
+// //svolgimento
+// let prezzoBiglietto;
+// let calcoloPrezzo;
+// let prezzoTotKm = kmTotali * prezzoKm;
+
+// if (etaPasseggero < 18){
+//     calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoMinori)
+// } else if (etaPasseggero > 65){
+//     calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoOver)
+// } else {
+//     calcoloPrezzo = prezzoTotKm
+// }
+
+// //output
+
+// prezzoBiglietto = calcoloPrezzo.toFixed(2);
+// console.log( ` Il prezzo totale è: ${prezzoBiglietto}€ ` );
+// alert ( ` Il prezzo totale è: ${prezzoBiglietto}€ ` );
