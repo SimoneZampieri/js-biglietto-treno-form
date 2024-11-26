1d9fc0f3c3a9be047318f637eea11c3790c1a943
SimoneZampieri <simonezampieri106@gmail.com>
2024-11-26 10:14:26 +0100

    [fix] JS

--- JS/script.js
+++ JS/script.js
@@ -4,34 +4,40 @@ const scontoOver = 40;
 const bottonGen = document.getElementById('genera');
 const bottonAnn = document.getElementById('annulla')
 
-//svolgimento
-
-//output
-
-
 //bottoni
 bottonGen.addEventListener('click', () =>{
     
     //dati da sapere
       const kmTotali = document.getElementById('inputTextKM');
       const etaPasseggero = document.getElementById('ageSelect');
+      const nome = document.getElementById('inputText').value
       const eta = etaPasseggero.value;
       const distanza = kmTotali.value;
       let prezzoBiglietto;
       let calcoloPrezzo;
       let prezzoTotKm = distanza * prezzoKm;
+      let tipoBiglietto = 'Standard'
 
      
      if (eta == 2){
           calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoMinori)
+            tipoBiglietto = 'Sconto Minori';
          } else if (eta == 3){
              calcoloPrezzo = (prezzoTotKm / 100) * (100 - scontoOver)
+            tipoBiglietto = 'Sconto Over';
          } else {
              calcoloPrezzo = prezzoTotKm
+             tipoBiglietto;
          }
         
          prezzoBiglietto = calcoloPrezzo.toFixed(2);
          console.log( ` Il prezzo totale è: ${prezzoBiglietto}€ ` );
+
+         //stampa biglietto
+         document.getElementById('nome-passeggero').innerHTML = nome
+         document.getElementById('offerta').innerHTML = tipoBiglietto
+         document.getElementById('prezzo-big').innerHTML = prezzoBiglietto
+
 
      })
 
@@ -40,5 +46,7 @@ bottonGen.addEventListener('click', () =>{
 const costoTot = document.getElementById('Costo')
 
 bottonGen.addEventListener('submit', () =>{
-    costoTot.innerHTML(prezzoBiglietto)
+    costoTot.innerText(`${prezzoBiglietto}`)
 })
+
+
